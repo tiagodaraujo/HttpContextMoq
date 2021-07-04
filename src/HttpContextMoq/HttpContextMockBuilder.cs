b611adaf5859903387c917e780c5ed9a61b5829f
Tiago Araujo <tiago.araujo@farfetch.com>
2021-07-04 22:44:13 +0100

    feat: HttpContextMockBuilder

---
+++ src/HttpContextMoq/HttpContextMockBuilder.cs
@@ -0,0 +1,36 @@
+﻿using HttpContextMoq.Extensions;
+
+namespace HttpContextMoq
+{
+    public class HttpContextMockBuilder
+    {
+        private string url;
+        private bool hasSession;
+
+        public HttpContextMockBuilder WithUrl(string url)
+        {
+            this.url = url;
+
+            return this;
+        }
+
+        public HttpContextMock Build()
+        {
+            var context = new HttpContextMock();
+
+            if (string.IsNullOrEmpty(this.url))
+            {
+                context.SetupUrl(this.url);
+            }
+
+            return context;
+        }
+
+        public static HttpContextMock Create(string url = null)
+        {
+            return new HttpContextMockBuilder()
+                .WithUrl(url)
+                .Build();
+        }
+    }
+}
