df9a6f67a09222b64cf206f1a0170f0a1a7bfd15
Tiago Araujo <tiago.araujo@farfetch.com>
2021-07-04 22:17:09 +0100

    feat: Header and Items Fakes

---
+++ src/HttpContextMoq/HeaderDictionaryFake.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace HttpContextMoq
+{
+    public class HeaderDictionaryFake : IHeaderDictionary
+    {
+        public StringValues this[string key] { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public long? ContentLength { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public ICollection<string> Keys => throw new System.NotImplementedException();
+
+        public ICollection<StringValues> Values => throw new System.NotImplementedException();
+
+        public int Count => throw new System.NotImplementedException();
+
+        public bool IsReadOnly => throw new System.NotImplementedException();
+
+        public void Add(string key, StringValues value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Add(KeyValuePair<string, StringValues> item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<string, StringValues> item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ContainsKey(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<string, StringValues>[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<string, StringValues>> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<string, StringValues> item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryGetValue(string key, [MaybeNullWhen(false)] out StringValues value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/HttpContextMoq/ItemsDictionaryFake.cs
@@ -0,0 +1,65 @@
+﻿namespace HttpContextMoq
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class ItemsDictionaryFake : IDictionary<object, object>
+    {
+        private IDictionary<object, object> _items = new Dictionary<object, object>();
+
+        object IDictionary<object, object>.this[object key]
+        {
+            get
+            {
+                if (_items.TryGetValue(key, out var value))
+                {
+                    return value;
+                }
+
+                return null;
+            }
+            set
+            {
+                _items[key] = value;
+            }
+        }
+
+        void IDictionary<object, object>.Add(object key, object value) => _items.Add(key, value);
+
+        bool IDictionary<object, object>.ContainsKey(object key) => _items.ContainsKey(key);
+
+        ICollection<object> IDictionary<object, object>.Keys => _items.Keys;
+
+        bool IDictionary<object, object>.Remove(object key) => _items.Remove(key);
+
+        bool IDictionary<object, object>.TryGetValue(object key, out object value) => _items.TryGetValue(key, out value);
+
+        ICollection<object> IDictionary<object, object>.Values => _items.Values;
+
+        void ICollection<KeyValuePair<object, object>>.Add(KeyValuePair<object, object> item) => _items.Add(item);
+
+        void ICollection<KeyValuePair<object, object>>.Clear() => _items.Clear();
+
+        bool ICollection<KeyValuePair<object, object>>.Contains(KeyValuePair<object, object> item) => _items.Contains(item);
+
+        void ICollection<KeyValuePair<object, object>>.CopyTo(KeyValuePair<object, object>[] array, int arrayIndex) => _items.CopyTo(array, arrayIndex);
+
+        int ICollection<KeyValuePair<object, object>>.Count => _items.Count;
+
+        bool ICollection<KeyValuePair<object, object>>.IsReadOnly => _items.IsReadOnly;
+
+        bool ICollection<KeyValuePair<object, object>>.Remove(KeyValuePair<object, object> item)
+        {
+            if (_items.TryGetValue(item.Key, out var value) && Equals(item.Value, value))
+            {
+                return _items.Remove(item.Key);
+            }
+
+            return false;
+        }
+
+        IEnumerator<KeyValuePair<object, object>> IEnumerable<KeyValuePair<object, object>>.GetEnumerator() => _items.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => _items.GetEnumerator();
+    }
+}
