b45c93d6586f5b032717f2bc06ac2a0d61886e7f
Tiago Araujo <tiago.araujo@farfetch.com>
2021-07-04 22:37:47 +0100

    feat: SetupUrl extension

---
+++ src/HttpContextMoq/Extensions/ContextExtensions.cs
@@ -0,0 +1,45 @@
+﻿namespace HttpContextMoq.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Http.Features;
+    using Microsoft.Extensions.Primitives;
+    using Moq;
+
+    public static class ContextExtensions
+    {
+        public static void SetupUrl(this HttpContextMock httpContextMock, string url)
+        {
+            var uri = new Uri(url);
+
+            httpContextMock.RequestMock.Mock.Setup(x => x.IsHttps).Returns(uri.Scheme == "https");
+            httpContextMock.RequestMock.Mock.Setup(x => x.Scheme).Returns(uri.Scheme);
+            if ((uri.Scheme == "https" && uri.Port != 443) || (uri.Scheme == "http" && uri.Port != 80))
+            {
+                httpContextMock.RequestMock.Mock.Setup(x => x.Host).Returns(new HostString(uri.Host, uri.Port));
+            }
+            else
+            {
+                httpContextMock.RequestMock.Mock.Setup(x => x.Host).Returns(new HostString(uri.Host));
+            }
+
+            httpContextMock.RequestMock.Mock.Setup(x => x.PathBase).Returns(string.Empty);
+            httpContextMock.RequestMock.Mock.Setup(x => x.Path).Returns(uri.AbsolutePath);
+            httpContextMock.RequestMock.Mock.Setup(x => x.QueryString).Returns(new QueryString(uri.Query));
+
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            var paramQuery = new Dictionary<string, StringValues>();
+            foreach (var key in query.AllKeys)
+            {
+                paramQuery.Add(key, query[key]);
+            }
+            httpContextMock.RequestMock.Query = new QueryCollection(paramQuery);
+
+            var requestFeature = new Mock<IHttpRequestFeature>();
+            requestFeature.Setup(x => x.RawTarget).Returns(uri.PathAndQuery);
+            httpContextMock.FeaturesMock.Mock.Setup(x => x.Get<IHttpRequestFeature>()).Returns(requestFeature.Object);
+        }
+    }
+}
